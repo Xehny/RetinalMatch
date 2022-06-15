3f58c0b303858c2f3e3d9b03295ac90ec926f26e
Xehny <105782952+Xehny@users.noreply.github.com>
2022-06-15 17:07:32 +1200

    Changes by bradp

--- src/RetinalMatch.java
+++ src/RetinalMatch.java
@@ -1,5 +1,7 @@
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
+import org.opencv.core.Rect;
+import org.opencv.core.Size;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 
@@ -25,5 +27,52 @@ public class RetinalMatch {
             System.err.println("Error: " + ex);
         }
 
+    }
+
+    public static Mat convertImage(Mat src) {
+        // Resize.
+        Mat resized = new Mat();
+        Size size = new Size(src.width() / 4, src.height() / 4);
+        Imgproc.resize(src, resized, size);
+
+        Mat grey = new Mat();
+        Imgproc.cvtColor(resized, grey, Imgproc.COLOR_RGB2GRAY);
+        Mat bw = new Mat();
+        Imgproc.threshold(grey, bw, 16, 255, Imgproc.THRESH_BINARY);
+
+        int left = bw.width() / 2;
+        int right = bw.width() / 2;
+        int top = bw.height() / 2;
+        int bottom = bw.height() / 2;
+
+        // Find furtherest left and right pixels which are not black
+        for(int row = 0; row < bw.height(); row++) {
+            for (int col = 0; col < bw.width(); col++) {
+                double[] pixel = bw.get(row, col);
+
+                // Check if pixel is not black.
+                if (pixel[0] > 0) {
+                    if (col < left) {
+                        left = col;
+                    }
+                    if (col > right) {
+                        right = col;
+                    }
+                    if (row < top) {
+                        top = row;
+                    }
+                    if (row > bottom) {
+                        bottom = row;
+                    }
+                }
+            }
+        }
+
+        // Crop the image.
+        Rect roi = new Rect(left, top, right - left + 1, bottom - top + 1);
+        Mat cropped = new Mat(resized, roi);
+
+        Mat dst = cropped;
+        return dst;
     }
 }
